7b380b2d0d63dbd23aef0d7d3a5c80c5f58aa9c6
Patrick Coffey <patrickcoffey48@gmail.com>
2018-03-01 15:13:58 -0600

    fix(lint): ignore extraneous deps errors

--- stories/index.js
+++ stories/index.js
@@ -1,6 +1,6 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
-import { action } from '@storybook/addon-actions'; // eslint-disable-line
+import { action } from '@storybook/addon-actions';
 import Button from '../src/components/Button/Button';
 
 storiesOf('Button', module).add('Toggle Button', () => (
