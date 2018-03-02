fedbd7e3f689240beb526857d3131cf21290f1aa
Patrick Coffey <patrickcoffey48@gmail.com>
2018-03-02 13:50:29 -0600

    feat(stories): clean up stories inclusion

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,13 +1,19 @@
 /**
- * @file config.js
- * Contains configuration for Storybook.
+ * @file index.js
+ * Contains stories for storybook.
  */
 
 import { configure } from '@storybook/react';
 
-function loadStories() {
-  require('../src/stories/index.js');
-  // You can require as many stories as you need.
-}
+/**
+ * Loads story files.
+ */
+const loadStories = () => {
+  // Fetch all *.story.js files from src/components subdirs.
+  // {@see https://webpack.js.org/guides/dependency-management/#require-context}.
+  const req = require.context('../src/components', true, /\.story\.js$/);
+  req.keys().forEach(req);
+};
 
+// Configure storybook.
 configure(loadStories, module);
