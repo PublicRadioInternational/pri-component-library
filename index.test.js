134e427d8e74c175a7fb5d4a3ac911a1b5e14cef
Luke Herrington <lherrington100@gmail.com>
2018-03-02 12:01:17 -0800

    test: better component export test

--- index.test.js
+++ index.test.js
@@ -3,9 +3,10 @@
  * Contains tests for index.js
  */
 
-import { Button as ButtonExp } from './index';
-import Button from './src/components/Button/Button';
+import { readdir } from 'fs-extra';
+import * as Components from './index';
 
-it('Exports all expected components', () => {
-  expect(ButtonExp).toBe(Button);
+it('Exports all expected components', async () => {
+  const dirContents = await readdir('./src/components');
+  expect(Object.keys(Components)).toEqual(dirContents);
 });
