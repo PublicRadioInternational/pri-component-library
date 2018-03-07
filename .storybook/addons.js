68b4923d9112e9bbc8ed88ad21b55b5fe8077fe3
Patrick Coffey <patrickcoffey48@gmail.com>
2018-03-07 15:34:12 -0600

    feat(a11y): add accessibility measurement tools

---
+++ .storybook/addons.js
@@ -0,0 +1,5 @@
+/**
+ * @file addons.js
+ * Configures addons for storybook.
+ */
+import '@storybook/addon-a11y/register';
--- src/components/Button/Button.story.js
+++ src/components/Button/Button.story.js
@@ -6,8 +6,11 @@
 import React from 'react';
 import { storiesOf } from '@storybook/react';
 import { action } from '@storybook/addon-actions';
+import { checkA11y } from '@storybook/addon-a11y';
 import Button from './Button';
 
-storiesOf('Button', module).add('Toggle Button', () => (
-  <Button onClick={action('button-clicked')}>Hello Button</Button>
-));
+storiesOf('Button', module)
+  .addDecorator(checkA11y)
+  .add('Toggle Button', () => (
+    <Button onClick={action('button-clicked')}>Hello Button</Button>
+  ));
