620ea94f5666618c1e8ef600c72234af58c495ea
Patrick Coffey <patrickcoffey48@gmail.com>
2018-03-07 15:29:35 -0600

    fix(button): small edits to the button component

--- src/components/Atoms/Button/Button.component.js
+++ src/components/Atoms/Button/Button.component.js
@@ -8,38 +8,61 @@ import PropTypes from 'prop-types';
 import styles from './Button.css';
 import { Dropdown } from '../Dropdown/Dropdown.component';
 
-function ifDropdown(dropdown, title, url, className) {
-  if (dropdown === false) {
+/**
+ * Renders a classic button.
+ */
+class Button extends Component {
+  static propTypes = {
+    dropdown: PropTypes.bool.isRequired,
+    title: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    className: PropTypes.oneOf(['btn', 'btnOrange'])
+  };
+
+  static defaultProps = {
+    className: 'btn'
+  };
+
+  /**
+   * Renders the button element.
+   *
+   * @returns {object} Renderable JSX.
+   */
+  renderButton = () => {
+    const { title, url, className } = this.props;
     return (
       <a href={url} className={styles[className]}>
         <span className="text-label">{title}</span>
       </a>
     );
-  }
+  };
 
-  return (
-    <div className="btn-group">
-      <a href={url} className={styles[className]}>
-        <span className="oi oi-media-play" ariaHidden="true" />
-        <span className="text-label">{title}</span>
-      </a>
-      <Dropdown />
-    </div>
-  );
-}
+  /**
+   * Renders the dropdown element.
+   *
+   * @returns {object} Renderable JSX.
+   */
+  renderDropDown = () => {
+    const { title, url, className } = this.props;
+    return (
+      <div className="btn-group">
+        <a href={url} className={styles[className]}>
+          <span className="oi oi-media-play" ariaHidden="true" />
+          <span className="text-label">{title}</span>
+        </a>
+        <Dropdown />
+      </div>
+    );
+  };
 
-class Button extends Component {
   render() {
-    const { dropdown, title, url, className } = this.props;
-    return ifDropdown(dropdown, title, url, className);
+    const { dropdown } = this.props;
+    if (dropdown) {
+      return this.renderDropdown();
+    }
+
+    return this.renderButton();
   }
 }
-
-Button.propTypes = {
-  dropdown: PropTypes.bool.isRequired,
-  title: PropTypes.string.isRequired,
-  url: PropTypes.string.isRequired,
-  className: PropTypes.string.isRequired
-};
 
 export default Button;
