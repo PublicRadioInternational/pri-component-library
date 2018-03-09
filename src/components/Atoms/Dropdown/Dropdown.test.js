fa881e02cf1d75947c58fbb9c491c351a0789917
Patrick Coffey <patrickcoffey48@gmail.com>
2018-03-08 18:07:52 -0600

    test(dropdown): test dropdown open and closing

--- src/components/Atoms/Dropdown/Dropdown.test.js
+++ src/components/Atoms/Dropdown/Dropdown.test.js
@@ -5,16 +5,26 @@
 
 import React from 'react';
 import renderer from 'react-test-renderer';
+import { mount } from 'enzyme';
 import Dropdown from './Dropdown.component';
-import DropdownItem from '../DropdownItem/DropdownItem.component';
 
 describe('<Dropdown />', () => {
+  const onClick = jest.fn();
+
   const dropdown = () => (
-    <Dropdown url="https://google.com" title="Listen" onClick={() => {}}>
-      <DropdownItem title="Google" url="https://google.com" />
-      <DropdownItem title="Custom Action" onClick={() => {}} />
-    </Dropdown>
+    <Dropdown url="https://google.com" title="Listen" onClick={onClick} />
   );
+
+  const wrapper = mount(dropdown());
+  it('Handles link click events', () => {
+    wrapper.find('a.btnGrpWhite').simulate('click');
+    expect(onClick).toHaveBeenCalledTimes(1);
+  });
+
+  it('Can be opened and closed', () => {
+    wrapper.find('button.btnDropdownWhite').simulate('click');
+    expect(wrapper.state().isOpen).toBe(true);
+  });
 
   it('Matches the snapshot', () => {
     const component = renderer.create(dropdown());
