f42d6c5fcb0d6b11ed531f2bebc927471fc09f84
Patrick Coffey <patrickcoffey48@gmail.com>
2018-03-20 11:01:14 -0500

    test: test search input changes

--- src/components/Molecules/Search/Search.test.js
+++ src/components/Molecules/Search/Search.test.js
@@ -5,23 +5,24 @@
 
 import React from 'react';
 import renderer from 'react-test-renderer';
-// import { mount } from 'enzyme';
+import { mount } from 'enzyme';
 import Search from './Search.component';
 
 describe('<Search />', () => {
-  // it('Handles input onChange events', () => {
-  //   const onChange = jest.fn();
-  //   const wrapper = mount(<Search />);
-  //   wrapper.find('input.searchText').simulate('change');
-  //   expect(onChange).toHaveBeenCalledTimes(1);
-  // });
-  //
   // it('Handles form submit events', () => {
   //   const onSubmit = jest.fn();
   //   const wrapper = mount(<Search />);
   //   wrapper.simulate('click');
   //   expect(onSubmit).toHaveBeenCalledTimes(1);
   // });
+
+  it('Handles input onChange events', () => {
+    const wrapper = mount(<Search />);
+    const input = wrapper.find('input[type="text"]');
+    input.instance().value = 'test';
+    input.simulate('change');
+    expect(wrapper.state('searchFieldValue')).toBe('test');
+  });
 
   it('Matches the snapshot', () => {
     const component = renderer.create(<Search baseUrl="http://google.com" />);
