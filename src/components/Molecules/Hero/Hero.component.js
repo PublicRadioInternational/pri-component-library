7720504d2dd24646acf30df170afbc395e94e6e7
Evan Willhite <evanmwillhite@562168.no-reply.drupal.org>
2018-03-28 12:38:19 -0500

    feat(hero): styling

--- src/components/Molecules/Hero/Hero.component.js
+++ src/components/Molecules/Hero/Hero.component.js
@@ -5,23 +5,30 @@
 
 import React from 'react';
 import PropTypes from 'prop-types';
+import styles from './Hero.css';
 
 import Icon from '../../Atoms/Svg/Icons.component';
 
 const Hero = ({ title, summary, hasAudio, category, imgSrc, imgAlt, url }) => (
-  <article typeof="sioc:Item foaf:Document">
-    <figure>
-      <img src={imgSrc} alt={imgAlt} />
+  <article typeof="sioc:Item foaf:Document" className={styles.hero}>
+    <figure className={styles.figure}>
+      <img className={styles.img} src={imgSrc} alt={imgAlt} />
     </figure>
-    <div>
+    <div className={styles.text}>
       {hasAudio && (
-        <a href={url}>
-          <Icon name="volume" />
+        <a className={styles.iconLink} href={url}>
+          <Icon name="volume" className={styles.icon} />
         </a>
       )}
-      <h1>{title}</h1>
-      <p>{summary}</p>
-      <strong>{category}</strong>
+      <h2>
+        <a className={styles.title} href={url}>
+          {title}
+        </a>
+      </h2>
+      <p className={styles.summary}>{summary}</p>
+      <a className={styles.category} href={url}>
+        {category}
+      </a>
     </div>
   </article>
 );
