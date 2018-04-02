c1385eaf78996283cf83ff13fff8d2d785c67b64
Rick Peterman <rpeterman@pri.org>
2018-04-02 16:36:37 -0400

    test(ctamessage): add snapshot test for showLogo
    - add test for logo size for loadUnder message
    - add snapshot test for title
    - add snapshot test for description
    - add snapshot test for action
    - add snapshot test for dismiss
    - add onClose handler called test for close button
    - add onClose handler called test for action button
    - add onClose handler called test for dismiss button

--- src/components/Molecules/CtaMessage/CtaMessage.test.js
+++ src/components/Molecules/CtaMessage/CtaMessage.test.js
@@ -5,7 +5,7 @@
 
 import React from 'react';
 import renderer from 'react-test-renderer';
-
+import { mount } from 'enzyme';
 import CtaMessage from './CtaMessage.component';
 
 describe('<CtaMessage />', () => {
@@ -21,5 +21,178 @@ describe('<CtaMessage />', () => {
       )
       .toJSON();
     expect(component).toMatchSnapshot();
+  });
+
+  it('Matches the CTA Message Logo snapshot', () => {
+    const component = renderer
+      .create(
+        <CtaMessage
+          data={{
+            name: 'test_1',
+            hash: '1234abcd',
+            showLogo: true
+          }}
+        />
+      )
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+  it('Logo is small for loadUnder', () => {
+    const wrapper = mount(
+      <CtaMessage
+        type="loadUnder"
+        data={{
+          name: 'test_1',
+          hash: '1234abcd',
+          showLogo: true
+        }}
+      />
+    );
+    expect(wrapper.find('.logo svg').prop('width')).toEqual(90);
+  });
+
+  it('Matches the CTA Message Title snapshot', () => {
+    const component = renderer
+      .create(
+        <CtaMessage
+          data={{
+            name: 'test_1',
+            hash: '1234abcd',
+            title: 'Test #1'
+          }}
+        />
+      )
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+  it('Matches the CTA Message Title snapshot', () => {
+    const component = renderer
+      .create(
+        <CtaMessage
+          data={{
+            name: 'test_1',
+            hash: '1234abcd',
+            title: 'Test title'
+          }}
+        />
+      )
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+  it('Matches the CTA Message Decription snapshot', () => {
+    const component = renderer
+      .create(
+        <CtaMessage
+          data={{
+            name: 'test_1',
+            hash: '1234abcd',
+            description: 'Test description...'
+          }}
+        />
+      )
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+  it('Matches the CTA Message Action button snapshot', () => {
+    const component = renderer
+      .create(
+        <CtaMessage
+          data={{
+            name: 'test_1',
+            hash: '1234abcd',
+            action: {
+              label: 'Do it',
+              url: 'https://www.google.com/'
+            }
+          }}
+        />
+      )
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+  it('Matches the CTA Message Dismiss button snapshot', () => {
+    const component = renderer
+      .create(
+        <CtaMessage
+          data={{
+            name: 'test_1',
+            hash: '1234abcd',
+            action: {
+              label: 'Do it',
+              url: 'https://www.google.com/'
+            },
+            dismiss: {
+              label: "Don't do it"
+            }
+          }}
+        />
+      )
+      .toJSON();
+    expect(component).toMatchSnapshot();
+  });
+
+  it('onPromptClose handler is called when close is click', () => {
+    const actionFn = jest.fn();
+    const wrapper = mount(
+      <CtaMessage
+        onClose={actionFn}
+        data={{
+          name: 'test_1',
+          hash: '1234abcd',
+          action: {
+            label: 'Do it',
+            url: 'https://www.google.com/'
+          }
+        }}
+      />
+    );
+    wrapper.find('button.close').simulate('click');
+    expect(actionFn).toHaveBeenCalled();
+  });
+
+  it('onPromptClose handler is called when action button is click', () => {
+    const actionFn = jest.fn();
+    const wrapper = mount(
+      <CtaMessage
+        onClose={actionFn}
+        data={{
+          name: 'test_1',
+          hash: '1234abcd',
+          action: {
+            label: 'Do it',
+            url: 'https://www.google.com/'
+          }
+        }}
+      />
+    );
+    wrapper.find('a.actionBtn').simulate('click');
+    expect(actionFn).toHaveBeenCalled();
+  });
+
+  it('onPromptClose handler is called when dismiss button is click', () => {
+    const actionFn = jest.fn();
+    const wrapper = mount(
+      <CtaMessage
+        onClose={actionFn}
+        data={{
+          name: 'test_1',
+          hash: '1234abcd',
+          action: {
+            label: 'Do it',
+            url: 'https://www.google.com/'
+          },
+          dismiss: {
+            label: "Don't do it"
+          }
+        }}
+      />
+    );
+    wrapper.find('button.dismissBtn').simulate('click');
+    expect(actionFn).toHaveBeenCalled();
   });
 });
