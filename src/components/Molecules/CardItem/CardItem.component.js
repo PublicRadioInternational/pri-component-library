c300f4cef92033c3528a5a0cef8d53529561403e
Evan Willhite <evanmwillhite@562168.no-reply.drupal.org>
2018-04-10 17:02:32 -0500

    PRIAPI-153: tests, hero

--- src/components/Molecules/CardItem/CardItem.component.js
+++ src/components/Molecules/CardItem/CardItem.component.js
@@ -3,14 +3,13 @@
  * Exports a card item component.
  */
 
-import 'intersection-observer'; // optional polyfill
-import Observer from '@researchgate/react-intersection-observer';
-import React, { Component } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import classNames from 'classnames/bind';
 import styles from './CardItem.css';
 
 import Icon from '../../Atoms/Svg/Icons.component';
+import LazyLoad from '../../Atoms/LazyLoad/LazyLoad.component';
 
 const cx = classNames.bind(styles);
 
@@ -61,132 +60,104 @@ BlurbContent.defaultProps = {
   className: null
 };
 
-const fetchImage = url =>
-  new Promise((resolve, reject) => {
-    const image = new Image();
-    image.src = url;
-    image.onload = resolve;
-    image.onerror = reject;
-  });
-
 /**
  * Component that renders a Card Item.
  */
-export default class CardItem extends Component {
-  static propTypes = {
-    url: PropTypes.string,
-    title: PropTypes.string,
-    imgSrc: PropTypes.string,
-    imgAlt: PropTypes.string,
-    blurb: PropTypes.node,
-    large: PropTypes.bool,
-    hasAudio: PropTypes.bool,
-    freeform: PropTypes.bool
-  };
+const CardItem = ({
+  url,
+  title,
+  imgSrc,
+  imgAlt,
+  blurb,
+  large,
+  hasAudio,
+  freeform
+}) => {
+  const largeClasses = element =>
+    cx({
+      [element]: true,
+      [`${element}Lg`]: large
+    });
 
-  static defaultProps = {
-    imgSrc: null,
-    imgAlt: null,
-    blurb: null,
-    url: null,
-    title: null,
-    large: false,
-    hasAudio: false,
-    freeform: false
-  };
+  const freeformClasses = element =>
+    cx({
+      [element]: freeform
+    });
 
-  loadImage = image => {
-    if (image.isIntersecting) {
-      const img = image.target;
-      const src = img.dataset.src; // eslint-disable-line
-      fetchImage(src).then(() => {
-        img.src = src;
-        img.removeAttribute('data-src');
-      });
-    }
-  };
-
-  render() {
-    const {
-      url,
-      title,
-      imgSrc,
-      imgAlt,
-      blurb,
-      large,
-      hasAudio,
-      freeform
-    } = this.props;
-    const largeClasses = element =>
-      cx({
-        [element]: true,
-        [`${element}Lg`]: large
-      });
-
-    const freeformClasses = element =>
-      cx({
-        [element]: freeform
-      });
-
-    return (
-      <article
-        className={largeClasses('cardItem')}
-        typeof="sioc:Item foaf:Document"
-      >
-        <div className={largeClasses('titleWrap')}>
-          {title && (
-            <h2
-              className={`${!large ? styles.title : ''} ${freeformClasses(
-                'freeformTitle'
-              )}`}
+  return (
+    <article
+      className={largeClasses('cardItem')}
+      typeof="sioc:Item foaf:Document"
+    >
+      <div className={largeClasses('titleWrap')}>
+        {title && (
+          <h2
+            className={`${!large ? styles.title : ''} ${freeformClasses(
+              'freeformTitle'
+            )}`}
+          >
+            <LinkedItem
+              url={url}
+              className={`${styles.link} ${freeformClasses('freeformLink')}`}
             >
-              <LinkedItem
-                url={url}
-                className={`${styles.link} ${freeformClasses('freeformLink')}`}
-              >
-                {title}
-              </LinkedItem>
-            </h2>
-          )}
-          {title && <span property="dc:title" content={title} />}
-          <span
-            property="sioc:num_replies"
-            content="0"
-            datatype="xsd:integer"
-          />
-        </div>
-        {imgSrc && (
-          <figure className={largeClasses('image')}>
-            <LinkedItem url={url}>
-              <Observer
-                onChange={this.loadImage}
-                onlyOnce
-                rootMargin="0% 0% 25%"
-              >
-                <img
-                  typeof="foaf:Image"
-                  data-src={imgSrc}
-                  alt={imgAlt}
-                  className={largeClasses('img')}
-                />
-              </Observer>
+              {title}
             </LinkedItem>
-          </figure>
+          </h2>
         )}
-        <BlurbContent freeform={freeform} className={largeClasses('blurb')}>
-          {blurb ? <div dangerouslySetInnerHTML={{ __html: blurb }} /> : null}
-          {hasAudio && (
-            <a className={styles.iconLink} href={url}>
-              <Icon
-                name="volume"
-                className={styles.icon}
-                isRoundIcon
-                ariaLabel="Audio"
+        {title && <span property="dc:title" content={title} />}
+        <span property="sioc:num_replies" content="0" datatype="xsd:integer" />
+      </div>
+      {imgSrc && (
+        <figure className={largeClasses('image')}>
+          <LinkedItem url={url}>
+            <LazyLoad>
+              <img
+                typeof="foaf:Image"
+                data-src={imgSrc}
+                alt={imgAlt}
+                className={largeClasses('img')}
               />
-            </a>
-          )}
-        </BlurbContent>
-      </article>
-    );
-  }
-}
+            </LazyLoad>
+          </LinkedItem>
+        </figure>
+      )}
+      <BlurbContent freeform={freeform} className={largeClasses('blurb')}>
+        {blurb ? <div dangerouslySetInnerHTML={{ __html: blurb }} /> : null}
+        {hasAudio && (
+          <a className={styles.iconLink} href={url}>
+            <Icon
+              name="volume"
+              className={styles.icon}
+              isRoundIcon
+              ariaLabel="Audio"
+            />
+          </a>
+        )}
+      </BlurbContent>
+    </article>
+  );
+};
+
+CardItem.propTypes = {
+  url: PropTypes.string,
+  title: PropTypes.string,
+  imgSrc: PropTypes.string,
+  imgAlt: PropTypes.string,
+  blurb: PropTypes.node,
+  large: PropTypes.bool,
+  hasAudio: PropTypes.bool,
+  freeform: PropTypes.bool
+};
+
+CardItem.defaultProps = {
+  imgSrc: null,
+  imgAlt: null,
+  blurb: null,
+  url: null,
+  title: null,
+  large: false,
+  hasAudio: false,
+  freeform: false
+};
+
+export default CardItem;
